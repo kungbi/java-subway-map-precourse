78f6c7687b57ea5420cefbf4349c8015abbb43b3
kungbi <kipper12335@gmail.com>
2024-11-15 00:20:54 +0900

    feat(InputView): 사용자 입력 함수들 구현
    입력 도움말과 함께 입력을 받는 함수들을 구현하였습니다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -2,11 +2,13 @@ package subway.view;
 
 public class InputView {
 
+    // 기능 선택 유저 입력 함수
     public String getCommand() {
         System.out.println("## 원하는 기능을 선택하세요.");
         return getUserInput();
     }
 
+    // 정류장 관련 입력 함수들
     public String getRegisterStationName() {
         System.out.println("## 등록할 역 이름을 입력하세요.");
         return getUserInput();
@@ -17,12 +19,50 @@ public class InputView {
         return getUserInput();
     }
 
-    public String getRemovedLineName() {
+    // 노선 관련 입력 함수들
+    public String getRegisterLineName() {
+        System.out.println("## 등록할 노선 이름을 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getRegisterLineStartStationName() {
+        System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getRegisterLineEndStationName() {
+        System.out.println("## 등록할 노선의 하행 종점역 이름을 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getRemoveLineName() {
         System.out.println("## 삭제할 노선 이름을 입력하세요.");
         return getUserInput();
     }
 
+    // 구간 관련 입력 함수들
 
+    public String getLineName() {
+        System.out.println("## 노선을 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getStationName() {
+        System.out.println("## 역이름을 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getOrderNumber() {
+        System.out.println("## 순서를 입력하세요.");
+        return getUserInput();
+    }
+
+    public String getRemoveSectionName() {
+        System.out.println("## 삭제할 구간의 노선을 입력하세요.");
+        return getUserInput();
+    }
+
+    // 유저 입력 받기 함수
     private String getUserInput() {
         System.out.print("> ");
         String input = Console.readline();
